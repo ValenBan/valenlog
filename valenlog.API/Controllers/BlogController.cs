f1cf55a483710639d89f84c651b72958b7c61609
ValenBan <banegasvalentin2003@gmail.com>
2025-05-19 18:37:37 -0300

    Create ApiResponse object [TASK 1.1.0]

--- valenlog.API/Controllers/BlogController.cs
+++ valenlog.API/Controllers/BlogController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using valenlog.Application.Common.Responses;
 using valenlog.Application.UCs.Blogs.Commands.AddBlog;
 using valenlog.Application.UCs.Blogs.Queries.GetAllBlogs;
 using valenlog.Domain.Entities;
@@ -21,14 +22,19 @@ namespace valenlog.API.Controllers
         [HttpGet]
         public async Task<IActionResult> GetAllBlogs()
         {
+            throw new Exception("Mamita posho, se rompio algo");
             var blogs = await _mediator.Send(new GetAllBlogsQuery());
-            return Ok(blogs);
+            return Ok(ApiResponse<List<Blog>>.Ok(blogs, "Blogs obtenidos con éxito"));
         }
 
         [HttpPost]
         public async Task<IActionResult> CreateBlog([FromBody] AddBlogCommand addBlogCommand)
         {
-            return Ok(await _mediator.Send(addBlogCommand));
+            //return Ok(await _mediator.Send(addBlogCommand));
+            var idBlogCreated = await _mediator.Send(addBlogCommand);
+
+            return Ok(ApiResponse<int>.Ok(idBlogCreated, $"Se creo el blog numero {idBlogCreated}"));
+
         }
     }
 }
---
+++ valenlog.Application/Common/Responses/ApiResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace valenlog.Application.Common.Responses
+{
+    public class ApiResponse<T>
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public T? Data { get; set; }
+        public List<string>? Errors { get; set; }
+
+        public static ApiResponse<T> Ok(T data, string? message = null) => new()
+        {
+            Success = true,
+            Message = message,
+            Data = data
+        };
+
+        public static ApiResponse<T> Fail(string? message = null, List<string>? errors = null) => new()
+        {
+            Success = false,
+            Message = message,
+            Errors = errors
+        };
+    }
+}
